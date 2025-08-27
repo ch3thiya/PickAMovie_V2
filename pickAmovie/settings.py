7383c5c14d8f90cb8ef70524302b05fcaf079e9e
ch3thiya <ac.ravindranathbc@gmail.com>
2025-08-27 10:08:17 +0530

    Configured secret key & hostname settings

--- pickAmovie/settings.py
+++ pickAmovie/settings.py
@@ -24,13 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7tyt9yd3(45!8+bhz-wagc2f)bn2bid)jq1ep(m&%ijwbl%z$2'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pickamovie.pythonanywhere.com']
 
 
 RENDER_EXTERNAL_HOSTNAME = os.getenv('RENDER_EXTERNAL_HOSTNAME')
